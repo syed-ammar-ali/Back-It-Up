38b469f5e6391dce76d88e39a67866b0c67415d1
Syed Ammar Ali <syedammarali12a@gmail.com>
2026-05-26 15:35:47 +0530

    gitignore updated

---
+++ config/cloudinary.config.js
@@ -0,0 +1,13 @@
+require("dotenv").config(); // Load environment variables from the .env file
+
+const cloudinary = require("cloudinary").v2;
+
+// Configure Cloudinary with the values from the .env file
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+  secure: true,
+});
+
+module.exports = cloudinary;
---
+++ models/files.model.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+const fileSchema = new mongoose.Schema({
+  path: {
+    type: String,
+    required: [true, "Path is required"],
+  },
+  orignalname: {
+    type: String,
+    required: [true, "Filename is required"],
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "users",
+    required: [true, "User is required"],
+  },
+});
+
+const file = mongoose.model("file", fileSchema);
+module.exports = file;
